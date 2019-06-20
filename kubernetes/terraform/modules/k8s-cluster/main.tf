e3e772b03af67bf5b37bf19ee5ea53a4137f59dc
Maksim Soldatov <mnsold.otus@gmail.com>
2019-06-20 20:24:23 +0300

    fix: terraform fmt

--- kubernetes/terraform/modules/k8s-cluster/main.tf
+++ kubernetes/terraform/modules/k8s-cluster/main.tf
@@ -1,5 +1,6 @@
 resource "google_container_cluster" "k8s_cluster" {
-  name     = "${var.k8s_cluster_name}"
+  name = "${var.k8s_cluster_name}"
+
   #https://www.terraform.io/docs/providers/google/d/google_container_engine_versions.html
   min_master_version = "${var.min_master_version}"
   enable_legacy_abac = "false"
@@ -8,29 +9,42 @@ resource "google_container_cluster" "k8s_cluster" {
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
   initial_node_count = 1
+
   remove_default_node_pool = true
 
   addons_config {
-    http_load_balancing { disabled = false }
-    horizontal_pod_autoscaling { disabled = false }
-    kubernetes_dashboard { disabled = false }
-    network_policy_config { disabled = true }
+    http_load_balancing {
+      disabled = false
+    }
+
+    horizontal_pod_autoscaling {
+      disabled = false
+    }
+
+    kubernetes_dashboard {
+      disabled = false
+    }
+
+    network_policy_config {
+      disabled = true
+    }
   }
 
   # отключить базовую авторизацию
   master_auth {
     username = ""
     password = ""
+
     client_certificate_config {
       issue_client_certificate = false
     }
   }
-
 }
 
 resource "google_container_node_pool" "k8s_cluster_nodes" {
-  name       = "${var.k8s_node_pool_name}"
-  cluster    = "${google_container_cluster.k8s_cluster.name}"
+  name    = "${var.k8s_node_pool_name}"
+  cluster = "${google_container_cluster.k8s_cluster.name}"
+
   #https://www.terraform.io/docs/providers/google/d/google_container_engine_versions.html
   #version = "1.12.7-gke.6"
 
@@ -38,7 +52,7 @@ resource "google_container_node_pool" "k8s_cluster_nodes" {
     preemptible  = true
     machine_type = "${var.machine_type}"
     disk_size_gb = "${var.disk_size_gb}"
-    disk_type = "${var.disk_type}"
+    disk_type    = "${var.disk_type}"
 
     metadata {
       disable-legacy-endpoints = "true"
@@ -54,15 +68,12 @@ resource "google_container_node_pool" "k8s_cluster_nodes" {
       "https://www.googleapis.com/auth/compute",
     ]
   }
-
   initial_node_count = "${var.initial_node_count}"
   autoscaling {
     min_node_count = "${var.min_node_count}"
     max_node_count = "${var.max_node_count}"
   }
-
   management {
     auto_repair = "true"
   }
-
 }
--- kubernetes/terraform/stage/main.tf
+++ kubernetes/terraform/stage/main.tf
@@ -8,22 +8,22 @@ provider "google" {
   version = "2.0.0"
   project = "${var.project}"
   region  = "${var.region}"
-  zone = "${var.zone}"
+  zone    = "${var.zone}"
 }
 
 module "k8s-cluster" {
-  source             = "../modules/k8s-cluster"
+  source = "../modules/k8s-cluster"
 
   #kubernetes cluster
-  k8s_cluster_name = "${var.k8s_cluster_name}"
+  k8s_cluster_name   = "${var.k8s_cluster_name}"
   min_master_version = "${var.min_master_version}"
 
   #kubernetes nodes pool
   k8s_node_pool_name = "${var.k8s_node_pool_name}"
-  machine_type = "${var.machine_type}"
-  disk_size_gb = "${var.disk_size_gb}"
-  disk_type = "${var.disk_type}"
+  machine_type       = "${var.machine_type}"
+  disk_size_gb       = "${var.disk_size_gb}"
+  disk_type          = "${var.disk_type}"
   initial_node_count = "${var.initial_node_count}"
-  min_node_count = "${var.min_node_count}"
-  max_node_count = "${var.max_node_count}"
+  min_node_count     = "${var.min_node_count}"
+  max_node_count     = "${var.max_node_count}"
 }
